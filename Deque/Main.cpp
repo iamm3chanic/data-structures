51cfe1fc5a3adaffa46bc04859f9e5f74f7fdd82
iamm3chanic <anfisa.lipko@gmail.com>
2021-05-02 12:39:59 +0300

    add Deque, inspired by @tjwrenn

---
+++ Deque/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // cout, endl
+
+#include "Deque.h"
+#include "Test.h"
+
+// ----
+// main
+// ----
+
+/**
+ * function main is a driver of function deque_test instantiated with class Deque
+ */
+int main () 
+{
+    using namespace std;
+    using namespace dt::prog::deque;
+    deque_test< Deque<int> >();
+    cout << "Done." << endl;
+    return 0;
+}
